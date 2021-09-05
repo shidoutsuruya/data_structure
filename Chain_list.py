6ffa7719f444b37155a5b8bcc937ce06b966a546
Max <shidoutsuruya@gmail.com>
2021-09-05 23:04:54 +0800

    sdsds

--- Chain_list.py
+++ Chain_list.py
@@ -44,7 +44,7 @@ class LinkedList:
             else:
                 current=current.next
         return found
-    def remove_203(self,item):
+    def removeElements(self,item):
         cur=dummy=Node()
         dummy.next=self.head
         while cur.next:
@@ -53,6 +53,19 @@ class LinkedList:
             else:
                 cur=cur.next
         return dummy.next
+    def removeNthFromEnd(self,index):
+        dummy=Node()
+        dummy.next=self.head
+        fast = slow = self.head
+        for _ in range(index):
+            fast = fast.next
+        if not fast:
+            return dummy.next.next
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return dummy.next   
     def order_search(self,item):
         current=self.head
         found=False
@@ -85,15 +98,57 @@ class LinkedList:
             previous.next=temp
    
                 
+             
+class solution():  
+    @staticmethod  
+    def py_list(head):
+        lst=[]
+        cur=head
+        while cur:
+            lst.append(cur.val)
+            cur=cur.next
+        return lst
+    @staticmethod  
+    def addTwoNumbers(l1_node,l2_node):
+        carry=0
+        cur=dummy=Node()
+        while l1_node or l2_node or carry:
+            v1=v2=0
+            if l1_node:
+                v1 = l1_node.val
+                l1_node = l1_node.next
+            if l2_node:
+                v2 = l2_node.val
+                l2_node = l2_node.next
+            carry, val = divmod(v1+v2+carry, 10)
+            cur.next = Node(val)
+            cur = cur.next
+        return dummy.next      
+    @staticmethod
+    def mergeTwoLists(l1_node,l2_node):
+        cur=dummy=Node(0)
+        while l1_node and l2_node:
+            if l1_node.val<l2_node.val:
+                cur.next=l1_node
+                l1_node=l1_node.next
+            else:
+                cur.next=l2_node
+                l2_node=l2_node.next
+            cur=cur.next
+        cur.next=l1_node or l2_node
+        return dummy.next
+   
                 
-        
         
         
 if __name__ == '__main__':
     test=LinkedList()
-    for i in [2,3,4,7,7,5,9]:
+    test2=LinkedList()
+    for i in [1,2,4,54]:
         test.append(i)
-    test.remove_203(1)
-    test.display()
-    
+    for j in [3,7,8]:
+        test2.append(j)
+    new_head=solution.mergeTwoLists(test.head,test2.head)
+    print(solution.hello(new_head))
+
     
