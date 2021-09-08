2baec206aa2add0124d3351d219605a8dc2951cb
Max <shidoutsuruya@gmail.com>
2021-09-08 13:47:16 +0800

    hello

--- Chain_list.py
+++ Chain_list.py
@@ -159,8 +159,122 @@ class LinkedList:
             while cur.next and cur.next.val==cur.val:
                 cur.next=cur.next.next
             cur=cur.next
+    def partition(self,x):
+        dummy1=l1=Node()
+        dummy2=l2=Node()
+        while self.head:
+            if self.head.val<x:
+                l1.next=self.head
+                l1=l1.next
+            else:
+                l2.next=self.head
+                l2=l2.next
+            self.head=self.head.next
+        l2.next=None
+        l1.next=dummy2.next
+        self.head=dummy1.next
+    def reverseList(self):
+        pre,cur=None,self.head
+        while cur:
+            nxt=cur.next
+            cur.next=pre
+            pre=cur
+            cur=nxt
+        self.head=pre
+    def reverseBetween(self,left,right):
+        if left==right:
+            return self.head
+        pre=dummy=Node()
+        dummy.next=self.head
+        for i in range(left-1):
+            pre=pre.next
+        #reverseList
+        reverse,cur=None,pre.next 
+        for i in range(right-left+1):
+            nxt=cur.next
+            cur.next=reverse
+            reverse=cur
+            cur=nxt
+        #end reverse list
+        pre.next,pre.next.next=cur,reverse
+        return dummy.next
+    @property
+    def hasCycle(self):
+        try:
+            slow,fast=self.head,self.head.next
+            while slow is not fast:
+                slow=slow.next
+                fast=fast.next.next
+            return True
+        except:
+            return False
+    def detectCycle(self):
+        slow,fast=self.head,self.head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next 
+            if slow==fast:
+                break
+        else:
+            return None
         
-             
+        cur=self.head
+        while cur!=fast:
+            cur=cur.next
+            fast=fast.next
+        return cur
+    def reorderList(self):
+        if not self.head or not self.head.next:
+            return self.head
+        def _splitList(head):
+            fast=slow=head 
+            while fast and fast.next:
+                slow=slow.next
+                fast=fast.next.next
+            middle=slow.next
+            slow.next=None
+            return head,middle
+        def _reverseList(head):
+            pre,cur=None,head
+            while cur:
+                nxt=cur.next
+                cur.next=pre
+                pre=cur
+                cur=nxt
+            return pre
+        def _mergeLists(l1,l2):
+            tail=head=l1 
+            l1=l1.next
+            while l2:
+                tail.next=l2
+                tail=tail.next
+                l2=l2.next
+                if l1:
+                    l1,l2=l2,l1
+            return head
+        l1,l2=_splitList(self.head)
+        l2=_reverseList(l2)
+        self.head=_mergeLists(l1,l2)
+    def insertSortList(self):
+        pre=dummy=Node()
+        cur=dummy.next=self.head
+        while cur and cur.next:
+            val=cur.next.val
+            if cur.val<val:
+                cur=cur.next
+                continue 
+            if pre.next.val>val:
+                pre=dummy
+            while pre.next.val<val:
+                pre=pre.next
+            new=cur.next
+            cur.next=new.next
+            new.next=pre.next
+            pre.next=new
+        return dummy.next
+    def SortList(self):
+        self.head=Tools.SortList(self.head)
+        
 class Tools():  
     @staticmethod  
     def py_list(head):
@@ -171,33 +285,33 @@ class Tools():
             cur=cur.next
         return lst
     @staticmethod  
-    def addTwoNumbers(l1_node,l2_node):
+    def addTwoNumbers(l1,l2):
         carry=0
         cur=dummy=Node()
-        while l1_node or l2_node or carry:
+        while l1 or l2 or carry:
             v1=v2=0
-            if l1_node:
-                v1 = l1_node.val
-                l1_node = l1_node.next
-            if l2_node:
-                v2 = l2_node.val
-                l2_node = l2_node.next
+            if l1:
+                v1 = l1.val
+                l1 = l1.next
+            if l2:
+                v2 = l2.val
+                l2 = l2.next
             carry, val = divmod(v1+v2+carry, 10)
             cur.next = Node(val)
             cur = cur.next
         return dummy.next      
     @staticmethod
-    def mergeTwoLists(l1_node,l2_node):
+    def mergeTwoLists(l1,l2):
         cur=dummy=Node(0)
-        while l1_node and l2_node:
-            if l1_node.val<l2_node.val:
-                cur.next=l1_node
-                l1_node=l1_node.next
+        while l1 and l2:
+            if l1.val<l2.val:
+                cur.next=l1
+                l1=l1.next
             else:
-                cur.next=l2_node
-                l2_node=l2_node.next
+                cur.next=l2
+                l2=l2.next
             cur=cur.next
-        cur.next=l1_node or l2_node
+        cur.next=l1 or l2
         return dummy.next
     @staticmethod
     def mergeKLists(lists):
@@ -208,16 +322,38 @@ class Tools():
         else:
             output=lists[0]
         for i in range(1,len(lists)):
-            output=solution.mergeTwoLists(output,lists[i])
+            output=Tools.mergeTwoLists(output,lists[i])
         return output
-    
-        
-        
+    @staticmethod
+    def SortList(head):
+        if not head or not head.next:
+            return head
+        pre,slow,fast=None,head,head
+        while fast and fast.next:
+            pre,slow,fast=slow,slow.next,fast.next.next
+        pre.next=None
+        return Tools.mergeTwoLists(Tools.SortList(head),Tools.SortList(slow))    
+    @staticmethod
+    def getInteresectionNode(l1,l2):
+        if l1 is None or l2 is None:
+            return None
+        cur1=l1  
+        cur2=l2   
+        while cur1 is not cur2:
+            if cur1:
+                cur1=cur1.next
+            else:
+                cur1=l2
+            if cur2:
+                cur2=cur2.next
+            else:
+                cur2=l1
+        return l1
 if __name__ == '__main__':
     test=LinkedList()
     test2=LinkedList()
-    for i in [1,1,1,2,3,4]:
+    for i in [1,5,2,5,31,1]:
         test.append(i)
     
-    test.removeDuplicates()
+    test.SortList()
     test.display()
